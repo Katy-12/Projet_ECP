cbe4ee0cd03c9f28baba0da6122d9d849d4435ad
coco <spykeez@hotmail.fr>
2019-12-04 09:11:48 +0100

    tst

---
+++ src/test/java/ecp/ecpprojet/EcpProjetApplicationTests.java
@@ -0,0 +1,13 @@
+package ecp.ecpprojet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcpProjetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
